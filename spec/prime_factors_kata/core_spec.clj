8385d237e84c4e8d1054fe71195cb85b54229904
Anda Cabrera <andarcabrera@gmail.com>
2015-12-15 09:28:02 -0600

    Add test #3

--- spec/prime_factors_kata/core_spec.clj
+++ spec/prime_factors_kata/core_spec.clj
@@ -7,4 +7,7 @@
     (should= [2] (primes-of 2)))
 
   (it "returns [3] for 3"
-    (should= [3] (primes-of 3))))
+    (should= [3] (primes-of 3)))
+
+  (it "returns [2 2] for 4"
+    (should= [2 2] (primes-of 4))))
--- src/prime_factors_kata/core.clj
+++ src/prime_factors_kata/core.clj
@@ -1,4 +1,9 @@
 (ns prime-factors-kata.core)
 
 (defn primes-of [number]
-  [number])
+  (loop [n number factor 2 result []]
+    (if (< n factor)
+      result
+    (if (= 0 (mod n factor))
+      (recur (/ n factor) factor (conj result factor))
+      (recur n (inc factor) result)))))
